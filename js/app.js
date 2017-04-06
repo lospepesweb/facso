ac6a54e729d24a7f282f2e637c35cceb513f132f
Andres Petronio <andpetronio@outlook.com>
2017-04-05 23:27:11 -0300

    a ver ahi

--- js/app.js
+++ js/app.js
@@ -16,6 +16,9 @@ $(document).ready(function(){
 function DeptoSinWeb(){
 			swal({
   				title: '<span class="informa_titulo">Sin sitio</span>',
+  				/*imageUrl: 'img/thumbs-up.jpg',
+  				imageWidth: 300,
+  				imageHeight: 300,*/
   				html: $('<div>')
     				.addClass('informa_texto')
     				.text('Este departamento no cuenta con sitio web propio.'),
@@ -36,12 +39,38 @@ $(function(){
 
         if(contenido.css("display")=="none"){ //open        
           contenido.slideDown(250);         
-          $(this).addClass("open"); 
+          $(this).addClass("open");
+          /*$(".carreras").css("height","100vh");*/
         }
         else{ //close       
           contenido.slideUp(250);
           $(this).removeClass("open");
+          /*$(".carreras").css("height","700px");  */
         }
         
       });
 });
+
+$(document).ready(main);
+ 
+var contador = 1;
+ 
+function main () {
+  $('.menu_bar').click(function(){
+    if (contador == 1) {
+      $('nav').animate({
+        left: '0'
+      });
+      contador = 0;
+    } else {
+      contador = 1;
+      $('nav').animate({
+        left: '-100%'
+      });
+    }
+  });
+  // Mostramos y ocultamos submenus
+  $('.submenu').click(function(){
+    $(this).children('.children').slideToggle();
+  });
+}
