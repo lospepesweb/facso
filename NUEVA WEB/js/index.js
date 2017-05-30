150f40ad754cdb191538bd1feea176caeeaafd58
Lucho Peñafiel <lucianopenafiel@gmail.com>
2017-05-30 10:33:38 -0300

    tuneando el menu hamburguesa

--- NUEVA WEB/js/index.js
+++ NUEVA WEB/js/index.js
@@ -5,7 +5,7 @@
 
 // FUNCIONES
 var seteo = function(){
-	var alturaPantalla = window.innerHeight - 100;
+	var alturaPantalla = window.innerHeight - 80;
 	var alturaTitulo = document.getElementById("titulo").clientHeight;
 	var margen = (alturaPantalla - alturaTitulo) / 2 ;
 
@@ -16,5 +16,15 @@ var seteo = function(){
 // EVENTOS
 seteo();
 window.addEventListener("resize", seteo);
+
+
+
+// -- ANIMACIÓN DEL MENU HAMBURGUESA -- //
+$(document).ready(function(){
+	$('#nav-iconLucho').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
 
 }())
